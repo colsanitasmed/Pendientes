302acbca5b1cd792609c4f39de11bdfdb52bd55b
colsanitasmed <miycruz@colsanitas.com>
2025-11-24 10:04:38 -0500

    Update API URL for document upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import datetime
 
 st.title("📄 Cargue de Documentos Pendientes")
 
-API_URL = "https://script.google.com/macros/s/AKfycbwgkgfRa5MkewfVFxdxhn46AIp1HrSSkn0A6rqpGV8qkhQK4hdWwa_d3hM_fxyBD0XX/exec"
+API_URL = "https://script.google.com/macros/s/AKfycbyZSENkkyPjHnY3RTbkixUlM4w9RoaMYzV0m_LhJnRxTIGaZPlWqzRWKjha-4bbVOT5/exec"
 
 archivo = st.file_uploader("Cargar Documento")
 
