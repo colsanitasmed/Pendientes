f582e2935c300090de90129f8c039c20abc08548
colsanitasmed <miycruz@colsanitas.com>
2025-11-24 08:52:38 -0500

    Refactor document upload process in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,34 @@
 import streamlit as st
-import requests
 import base64
+import requests
+import datetime
 
 st.title("📄 Cargue de Documentos Pendientes")
 
-API_URL = "https://script.google.com/macros/s/AKfycbweOuq4IBBS58cemM_H35zc7sUnz0bVQPnlggK18eziLppYejiY15hU8sjG1cYighEN/exec"   # <-- reemplazar por la URL del despliegue
+API_URL = "https://script.google.com/macros/s/AKfycbwIH-E6KaiQzKMIiagOPrKtkSO6cC0lcVGueC-71VtNgEL0QeUCBBbbQGxnAkyG4cGKww/exec"
 
-# 1. Tipo de documento
-tipo = st.selectbox("Tipo de Documento", ["PDF", "JPG", "PNG", "Otros"])
-
-# 2. Cargar archivo
 archivo = st.file_uploader("Cargar Documento", type=None)
 
 if archivo is not None:
-    st.success("Archivo cargado. Listo para procesar.")
+    st.success("Archivo listo para procesar")
 
-    if st.button("Procesar archivo"):
-        # Convertir archivo a base64
-        archivo_b64 = base64.b64encode(archivo.getvalue()).decode()
+    if st.button("Enviar"):
+        base64_file = base64.b64encode(archivo.getvalue()).decode()
 
         data = {
-            "accion": "ocr",
-            "tipo": tipo,
             "nombre_archivo": archivo.name,
-            "archivo_base64": archivo_b64
+            "mime": archivo.type,
+            "base64": base64_file,
+
+            "FechaCarga": str(datetime.date.today()),
+            "DocumentoPaciente": "",
         }
 
-        # Enviar como JSON
         response = requests.post(API_URL, json=data)
 
-        # Validar respuesta
         try:
             resultado = response.json()
+            st.json(resultado)
         except:
-            st.error("⚠️ La API respondió algo que NO es JSON.")
+            st.error("La API devolvió algo que no es JSON")
             st.write(response.text)
-            st.stop()
-
-        if resultado.get("estado") == "ok":
-            st.subheader("Datos extraídos del archivo 📄")
-            st.json(resultado["data"])
-        else:
-            st.error("❌ Error en la API")
-            st.write(resultado)
